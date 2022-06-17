382e483738e1fee78897c11174ffa1b6db146465
Fabian Kresler <fabi.kresler@googlemail.com>
2022-06-17 22:58:44 +0200

    fix: fix selection of achievement tabs data

--- components/AchievementBacklog.tsx
+++ components/AchievementBacklog.tsx
@@ -4,64 +4,60 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
 import AchievementBlock from './AchievementBlock';
-import { AchievementTracker } from '../types/achievements';
-
-enum AchievementBacklogLabel {
-  'backlog-tab-weekly',
-  'backlog-tab-lifetime',
-  'backlog-tab-seasonal',
-  'backlog-tab-weapons',
-  'backlog-tab-builds',
-}
+import { AchievementGroup, AchievementTracker } from '../types/achievements';
 
 type AchievementBacklogDefinition = {
-  [key in AchievementBacklogLabel]: {
+  [key in AchievementGroup]: {
     index: number;
+    label: AchievementGroup;
   }
-};
-
-const ACHIEVEMENT_BACKLOG_TABS: AchievementBacklogDefinition = {
-  [AchievementBacklogLabel['backlog-tab-weekly']]: {
-    index: 0,
-  },
-  [AchievementBacklogLabel['backlog-tab-lifetime']]: {
-    index: 1,
-  },
-  [AchievementBacklogLabel['backlog-tab-seasonal']]: {
-    index: 2,
-  },
-  [AchievementBacklogLabel['backlog-tab-weapons']]: {
-    index: 3,
-  },
-  [AchievementBacklogLabel['backlog-tab-builds']]: {
-    index: 4,
-  },
 };
 
 interface TabPanelProps {
   children?: React.ReactNode;
-  label: AchievementBacklogLabel;
-  selectedIndex: number;
+  label: AchievementGroup;
+  isHidden: boolean;
 }
 
 interface AchievementBacklogProps {
   data?: AchievementTracker[];
 }
 
+const ACHIEVEMENT_BACKLOG_TABS: AchievementBacklogDefinition = {
+  Weekly: {
+    index: 0,
+    label: 'Weekly',
+  },
+  Seasonal: {
+    index: 1,
+    label: 'Seasonal',
+  },
+  Lifetime: {
+    index: 2,
+    label: 'Lifetime',
+  },
+  Weapons: {
+    index: 3,
+    label: 'Weapons',
+  },
+  Builds: {
+    index: 4,
+    label: 'Builds',
+  },
+};
+
 function TabPanel({
-  label, selectedIndex, children = null,
+  label, isHidden, children = null,
 }: TabPanelProps) {
-  const { index } = ACHIEVEMENT_BACKLOG_TABS[label];
-  const hidden = index !== selectedIndex;
-  const id = AchievementBacklogLabel[label];
+  const id = `backlog-tab-${label}`;
   return (
     <div
       role="tabpanel"
-      hidden={hidden}
+      hidden={isHidden}
       id={id}
       aria-labelledby={id}
     >
-      {!hidden && (
+      {!isHidden && (
         <Box sx={{ p: 3 }}>
           <Typography>{children}</Typography>
         </Box>
@@ -80,27 +76,20 @@ function AchievementBacklog({ data }: AchievementBacklogProps) {
   return (
     <div>
       <Tabs value={selectedTab} onChange={handleChange} aria-label="Achievement Backlog Tabs" variant="fullWidth">
-        <Tab label="Weekly" id="backlog-tab-weekly" aria-controls="backlog-tabpanel-weekly" />
-        <Tab label="Seasonal" id="backlog-tab-seasonal" aria-controls="backlog-tabpanel-seasonal" />
-        <Tab label="Lifetime" id="backlog-tab-lifetime" aria-controls="backlog-tabpanel-lifetime" />
-        <Tab label="Weapons" id="backlog-tab-weapons" aria-controls="backlog-tabpanel-weapons" />
-        <Tab label="Build" id="backlog-tab-builds" aria-controls="backlog-tabpanel-builds" />
+        {Object.entries(ACHIEVEMENT_BACKLOG_TABS).map(([, value]) => (
+          <Tab
+            key={value.label}
+            label={value.label}
+            id={`backlog-tab-${value.label}`}
+            aria-controls={`backlog-tabpanel-${value.label}`}
+          />
+        ))}
       </Tabs>
-      <TabPanel label={AchievementBacklogLabel['backlog-tab-weekly']} selectedIndex={selectedTab}>
-        <AchievementBlock data={data} filter="Weekly" />
-      </TabPanel>
-      <TabPanel label={AchievementBacklogLabel['backlog-tab-seasonal']} selectedIndex={selectedTab}>
-        <AchievementBlock data={data} filter="Seasonal" />
-      </TabPanel>
-      <TabPanel label={AchievementBacklogLabel['backlog-tab-lifetime']} selectedIndex={selectedTab}>
-        <AchievementBlock data={data} filter="Lifetime" />
-      </TabPanel>
-      <TabPanel label={AchievementBacklogLabel['backlog-tab-weapons']} selectedIndex={selectedTab}>
-        <AchievementBlock data={data} filter="Weapons" />
-      </TabPanel>
-      <TabPanel label={AchievementBacklogLabel['backlog-tab-builds']} selectedIndex={selectedTab}>
-        <AchievementBlock data={data} filter="Builds" />
-      </TabPanel>
+      {Object.entries(ACHIEVEMENT_BACKLOG_TABS).map(([, value]) => (
+        <TabPanel key={value.label} label={value.label} isHidden={value.index !== selectedTab}>
+          <AchievementBlock data={data} filter={value.label} />
+        </TabPanel>
+      ))}
     </div>
   );
 }
